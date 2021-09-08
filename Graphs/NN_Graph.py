74aaa02d655f3f2122418519e0d12401fe94e484
Simonws92 <43178680+Simonws92@users.noreply.github.com>
2021-09-08 14:54:01 +0200

    Add files via upload

---
+++ Graphs/NN_Graph.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 13:45:02 2021
+
+@author: Simon
+"""
+
+import numpy as np
+import new_Dense_1_3 as nd
+import copy
+
+
+#x = np.array([ XOR , XOR, XOR ])
+H = [3,5,3,3]
+W,b = nd.init_weights(H, A = 0)
+b[0] = np.zeros((5,1))
+b[1] = np.zeros((3,1))
+b[2] = np.zeros((3,1))
+
+def neural_net(W,b,H,x):
+    H_act = ['tanh']*3 + ['I']
+    z,a = nd.feedforward( x, H, W, b,H_act=H_act)
+    return a[-1]
+
+X_c = [ np.random.random((3,1)) ]*2 + [ np.zeros((3,1)) ]*3
+
+"certain x_values may want to be fixed and not reset after every time-step"
+x_fixed = [1,1,0,0,0]
+x_computed = [0]*5
+
+Z = [0]*5
+
+A = np.array([ [0,0,0,0,0],
+               [0,0,0,0,0],
+               [1,1,0,1,1],
+               [0,0,1,0,0],
+               [0,0,1,0,0] ])
+
+"pseudo timesteps"
+T=10
+
+node_sum = [0]*5
+
+"timestep / stages"
+for c in range(2):
+    
+    "Z are the outputs"
+    Z = [0]*5
+    
+    "X are the inputs"
+    X_c = [ np.random.random((3,1)) ]*2 + [ np.zeros((3,1)) ]*3
+    X = copy.deepcopy(X_c)
+    print("NEXT TIMESTEP")
+    "pseudo-timestep"
+    for t in range(T):
+        #print(X)     
+        for i in range(5):
+            for j in range(5):
+                if A[i,j]!=0 and x_computed[j]==0:
+                    #print(i+1,j+1)
+                    Z[j] = neural_net(W,b,H, X[j] )
+                    X[i] += Z[j]
+                    #print(Z[j])
+#                    if j == 2:
+#                        print("Output from 3", i,j)
+#                        print(Z[j])
+                    
+                    "prevents computations of certain inputs again"
+                    if x_fixed[j] == 1:
+                        x_computed[j] = 1
+                        
+                else:
+                    pass
+    
+        
+        for I in range(5):
+            node_sum[I] += np.sum(X[I])
+            
+    "resets computed rule"
+    x_computed = [0]*5
+    
+    print("Output at stage:", c, np.round(np.array(Z),3)  )
+
+print("X_input", np.round(np.array(X),3))
+
+
+
+
+
+
+
