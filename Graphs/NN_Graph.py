9c57087f000cb157dc90dae1c5218213b7b2a5aa
Simonws92 <43178680+Simonws92@users.noreply.github.com>
2021-09-13 12:07:14 +0200

    Add files via upload

--- Graphs/NN_Graph.py
+++ Graphs/NN_Graph.py
@@ -11,14 +11,42 @@ import copy
 
 
 #x = np.array([ XOR , XOR, XOR ])
+
+"Define graph architecture"
+A = np.array([ [0,0,0,0,0],
+               [0,0,0,0,0],
+               [1,1,0,1,1],
+               [0,0,1,0,0],
+               [0,0,1,0,0] ])
+    
+WW = np.array([ [0,0,0,0,0],
+                [0,0,0,0,0],
+                [1,1,1,1,1],
+                [0,0,1,0,0],
+                [0,0,1,0,0] ])
+
+Graph_weights = [ [0,0]*5 for i in range(5) ]
+
+"Load weights"
 H = [3,5,3,3]
 W,b = nd.init_weights(H, A = 0)
 b[0] = np.zeros((5,1))
 b[1] = np.zeros((3,1))
 b[2] = np.zeros((3,1))
 
+for i in range(5):
+    for j in range(5):
+        if WW[i,j]==1:
+            k = np.random.randint(2,10)                                        #number of layers
+            H = [3] + [ np.random.randint(5,10) for i in range(k) ] + [3]      #number of neurons
+            
+            W, b = nd.init_weights(H, A = 0)
+            Graph_weights[i][2*j]   = W
+            Graph_weights[i][2*j+1] = H
+            
+
 def neural_net(W,b,H,x):
-    H_act = ['tanh']*3 + ['I']
+    H_act = ['tanh']*(len(H)-2) + ['I']
     z,a = nd.feedforward( x, H, W, b,H_act=H_act)
     return a[-1]
 
@@ -30,11 +58,7 @@ x_computed = [0]*5
 
 Z = [0]*5
 
-A = np.array([ [0,0,0,0,0],
-               [0,0,0,0,0],
-               [1,1,0,1,1],
-               [0,0,1,0,0],
-               [0,0,1,0,0] ])
+
 
 "pseudo timesteps"
 T=10
@@ -58,7 +82,7 @@ for c in range(2):
             for j in range(5):
                 if A[i,j]!=0 and x_computed[j]==0:
                     #print(i+1,j+1)
-                    Z[j] = neural_net(W,b,H, X[j] )
+                    Z[j] = neural_net( Graph_weights[i][2*j] ,b, Graph_weights[i][2*j+1] , X[j] )
                     X[i] += Z[j]
                     #print(Z[j])
 #                    if j == 2:
