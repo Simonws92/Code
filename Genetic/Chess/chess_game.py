7de8a18d398106ad97f6bba6bb4b7ad9c4a79f4c
Simonws92 <43178680+Simonws92@users.noreply.github.com>
2023-08-16 10:45:59 +0200

    Add files via upload

--- Genetic/Chess/chess_game.py
+++ Genetic/Chess/chess_game.py
@@ -4,10 +4,11 @@ Created on Tue Jul 16 21:15:50 2019
 
 @author: Simon
 """
-start = time.time()
+
 import numpy as np
 import time
 import pygame
+import os
 
 import pyautogui
 import cv2
@@ -24,6 +25,7 @@ import chess_rules as chess
 #import tkinter as tk
 #from tkinter import messagebox
 
+start = time.time()
 
 TRAINING = True
 NR_CH   = 5
@@ -34,7 +36,8 @@ Output = 32 #Choose a piece and choose where to move
 H = [Input, 20,Output] #1 input, 2 hidden, 1 output
 #B=1
 
-restart = True
+restart = False
+
 if restart == True:
     CH_W = []
     CH_B = []
@@ -56,49 +59,113 @@ def PIECE_LIST():
     
     
     List_white = [["p",16,1,48],["p",17,1,49],["p",18,1,50],["p",19,1,51],["p",20,1,52],["p",21,1,53],["p",22,1,54],["p",23,1,55],
-                  ["r",24,1,56],["r",25,1,57],["kn",26,1,58],["kn",27,1,59],["b",28,1,60],["b",29,1,61],["q",30,1,62],["k",31,1,63]]
-    
-    
-
+                  ["r",24,1,56],["r",25,1,63],["kn",26,1,57],["kn",27,1,62],["b",28,1,58],["b",29,1,61],["q",30,1,59],["k",31,1,60]]
     
     return List_black, List_white
 
 
 
 
-def compile_moves(player, AV_pieces, board):
+def compile_moves(player, AV_pieces, board, groups):
+    """
+    Compiles a list with all legal moves for every piece.
+    
+    groups list of boolean 0, 1: [pawn , rook ,knight ,bishop , queen, king]
+    """
     legal_moves = np.zeros((32,27))
     for piece_slot in range(16):
         current_loc = AV_pieces[player][piece_slot][-1]
         piece_id = piece_slot + 16*(player) #If white player turns. The piece id will increment by 15.
         
-        if piece_id <= 7 or  (piece_id <= 23 and piece_id >= 16):
+        if (piece_id <= 7 or  (piece_id <= 23 and piece_id >= 16)) and groups[0]==1:
             #print(piece_id, current_loc, "id and loc")
             piece = "pawn"
             board, moves, legal_moves = chess.PAWN(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
         
-        if piece_id == 8 or piece_id == 9 or piece_id == 24 or piece_id == 25:
+        if (piece_id == 8 or piece_id == 9 or piece_id == 24 or piece_id == 25) and groups[1]==1:
             piece = "rook"
             board, moves, legal_moves = chess.ROOK(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
-         
-        if piece_id == 12 or piece_id == 13 or piece_id == 28 or piece_id == 29:
-            piece = "bishop"
-            board, moves, legal_moves = chess.BISHOP(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
         
-        if piece_id == 10 or piece_id == 11 or piece_id == 26 or piece_id == 27:
+        if (piece_id == 10 or piece_id == 11 or piece_id == 26 or piece_id == 27) and groups[2]==1:
+            #print("Knight")
             piece = "knight"
             board, moves, legal_moves = chess.KNIGHT(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
         
-        if piece_id == 14 or piece_id == 30:
+        if (piece_id == 12 or piece_id == 13 or piece_id == 28 or piece_id == 29) and groups[3]==1:
+            #print("Bishop")
+            piece = "bishop"
+            board, moves, legal_moves = chess.BISHOP(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
+        
+        if (piece_id == 14 or piece_id == 30) and groups[4]==1:
             piece = "queen"
             board, moves, legal_moves = chess.KING_QUEEN(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)    
             
-        if piece_id == 15 or piece_id == 31:
+        if (piece_id == 15 or piece_id == 31) and groups[5]==1:
             piece = "king"
             board, moves, legal_moves = chess.KING_QUEEN(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)  
-    #print(legal_moves)
+    
     return board, legal_moves#, piece
 
+
+def Player_move(player_turn, legal_moves, AV_pieces, board, groups,
+                H, W, B, dp, fitness ):
+    
+    _, decision = br.feedforward(legal_moves, H, W, B, dp)
+    "Here we multiply elementwise decision with binary legal_moves to get the full legal decision list"
+    legal_moves_binary = legal_moves.copy()
+    legal_moves_binary[legal_moves_binary > 0] = 1
+    
+    decision *= legal_moves_binary
+    #print(decision)
+    "Now choose piece based on legal decision"
+    old_max = 0
+    current_max = 0
+    for i in range(16): #Goes through all 32 pieces to see which one is the best decision...
+        current_max = sum(decision[i+16*player_turn])
+        if current_max > old_max:
+            piece_choice = i+16*player_turn #which piece we choose
+            old_max = current_max
+    
+    
+    "here we choose the most likely move post legal-move check"
+    index_nr = list( decision[piece_choice] ).index( max( decision[piece_choice] ) )
+    next_loc = int(legal_moves[piece_choice,index_nr])
+    
+    
+    "Play the selected piece to desired location"
+    
+    "Takes piece placement information"
+    "Since the list have 16 slots, we must convert from 32 to 16 etc"
+    current_loc = AV_pieces[player_turn][piece_choice - player_turn*16][-1]
+    piece = AV_pieces[player_turn][piece_choice - player_turn*16][0]
+    
+    "Updates piece placement information"
+    AV_pieces[player_turn][piece_choice - player_turn*16][-1] = next_loc 
+    
+    if board[next_loc][-1] == 1:
+        #print("Piece captured!")
+        fitness+=1
+    
+    next_row = int( np.floor(next_loc / 8)   )      #Zero based
+    next_col = int( next_loc - 8*next_row )      #Zero based
+    
+    #print(player_turn, piece_choice, piece," to:", next_row, next_col, next_loc)
+    #print("from:", current_loc, "to", next_loc)
+    
+    board[next_loc] = (0,0,0,0,0,0,0,0)
+    board[next_loc] = board[current_loc].copy()
+    #board[next_loc, 4] = next_loc
+    board[current_loc] = (0,0,0,0,0,0,0,0) #leaves a blank tile when leaving
+    
+    board_gp = board[:,:3]
+    board_gp = np.reshape(board_gp, (8,8,3))
+    
+    plt.clf()
+    plt.imshow(board_gp)
+    plt.pause(0.01)
+    
+    
+    return board, AV_pieces, fitness
 
 def play_game(H, W, B, W2, B2, s):
     points = 0
@@ -113,6 +180,10 @@ def play_game(H, W, B, W2, B2, s):
     #7: occupied
     #8: id
     
+    # pawn, rook, knight, bishop, queen, king
+    #groups = [1,1,0,0,0,0] # Binary list of allowed piece-groups
+    groups = [1,1,1,1,1,1]
+    
     board = chess.LOAD_CHESS()          #Loads the starting board
     black_p, white_p = PIECE_LIST()     #Loads the starting piece_lists
     AV_pieces = [ black_p, white_p ]
@@ -120,84 +191,36 @@ def play_game(H, W, B, W2, B2, s):
     captured = 0
     
     old_max = -1
-    "choice is the id of the piece"
+    "choice is the id of the piece. Every piece have their own id"
     choice = 0 
     choice_move = 0
     
-    for I in range(3,100): #Each character gets 100 turns each
+    #print()
+    #print("Start")
+    for I in range(1,71): #Each character gets 100 turns each
+        "Player 1 (Bottom) starts. Player 0 is second"
         #print("TOTAL ITERATION", I)
         
+        
+        ###
+        ### NEW
+        ###
+        
+        "Checks if player have any legal moves"
         player_turn = I%2
         #print("PLAYER:" ,player_turn)
-        board, legal_moves = compile_moves(player_turn, AV_pieces, board)
-        #legal_move_player = legal_moves[player_turn*16:(player_turn+1)*16]
-        
-        #print(legal_moves)
-        #print("len", shape(legal_moves)) #16x27
-        
-        _, decision = br.feedforward(legal_moves, H, W, B, dp)
-        "Here we multiply elementwise decision with binary legal_moves to get the full legal decision list"
-        legal_moves_binary = legal_moves.copy()
-        legal_moves_binary[legal_moves_binary > 0] = 1
-        
-        decision *= legal_moves_binary
-        #print(decision)
-        "Now choose piece based on legal decision"
-        old_max = 0
-        current_max = 0
-        for i in range(16): #Goes through all 32 pieces to see which one is the best decision...
-            current_max = sum(decision[i+16*player_turn])
-            if current_max > old_max:
-                piece_choice = i+16*player_turn #which piece we choose
-                old_max = current_max
+        board, legal_moves = compile_moves(player_turn, AV_pieces, board, groups)
         
         
-        "here we choose the most likely move post legal-move check"
-        index_nr = list( decision[piece_choice] ).index( max( decision[piece_choice] ) )
-        next_loc = int(legal_moves[piece_choice,index_nr])
-
-        
-        "Play the selected piece to desired location"
-        
-        "Takes piece placement information"
-        current_loc = AV_pieces[player_turn][piece_choice - player_turn*16][-1]
-        piece = AV_pieces[player_turn][piece_choice - player_turn*16][0]
-        
-        
-        "Updates piece placement information"
-        AV_pieces[player_turn][piece_choice - player_turn*16][-1] = next_loc 
-        
-        
-        
-        "we change choice to choice -1 since temp_board is zero based"
-        #choice-=1 
-        #We move selected piece to a location
-        #print()
-        #if choice == choice:# or choice == 9 or choice == 24 or choice == 25:
-        #print("piece:", piece)
-            #print("current_loc:",current_loc, "next_loc:", next_loc)
-        if board[next_loc][-1] == 1:
-            #print("Piece captured!")
-            fitness+=1
-        
-        board[next_loc] = board[current_loc]
-        board[next_loc, 4] = next_loc
-        
-        board[current_loc] = (0,0,0,0,0,0,0,0) #leaves a blank tile when leaving
-        #print("Rook goes to:", next_loc)
-        
-        
-        board_gp = board[:,:3]
-        board_gp = np.reshape(board_gp, (8,8,3))
-        clf()
-        imshow(board_gp)
-        plt.pause(0.01)
-        
-        choice = 0 #Reset value
-
-    #print("captured pieces:", fitness)
+        if np.average(legal_moves) == 0:
+            #print("We cannot move anywhere! We have lost!")
+            #print("Rounds:", I, "Fitness:", fitness)
+            return fitness
+        else:
+            board, AV_pieces, fitness = Player_move(player_turn, legal_moves, AV_pieces, board, groups, H, W, B, dp, fitness )
+            
     return fitness
-
+    
 
 
 
@@ -257,7 +280,7 @@ def start_game(NR_CH, H, CH_W, CH_B, MUTATION_COEFF ,TRAINING ):
 
 
 
-GENERATIONS = 100
+GENERATIONS = 1
 
 if restart == True:
     fit = []
@@ -265,14 +288,16 @@ if restart == True:
     hs_max = []
 
 for i in range(GENERATIONS):
-    new_weights, new_bias, Fitness_list= start_game(NR_CH, H, CH_W, CH_B, MUTATION_COEFF, TRAINING)
+    new_weights, new_bias, Fitness_list = start_game(NR_CH, H, CH_W, CH_B, MUTATION_COEFF, TRAINING)
     CH_W = new_weights
     CH_B = new_bias
-    print( "Max fitness after generation", i+1, ": " ,  max(Fitness_list), " at index: ", list(Fitness_list).index(max(Fitness_list)))
+    #print( "Max fitness after generation", i+1, ": " ,  max(Fitness_list), " at index: ", list(Fitness_list).index(max(Fitness_list)))
 
     
     fit.append( max(Fitness_list) )
 
+plt.figure()
+plt.plot(fit)
 
 
 end = time.time()
@@ -321,6 +346,10 @@ plt.show()
 '''
 
 
+
+    
+    
+    
 
 
 
--- Genetic/Chess/chess_rules.py
+++ Genetic/Chess/chess_rules.py
@@ -7,13 +7,15 @@ Created on Tue Apr  7 20:26:00 2020
 import numpy as np
 
 def LOAD_CHESS(): #Loads the pieces in correct positions
-    #Placing black pieces
     #notations
-    #first three determines type of piece
-    #4th is black 0 or white 1 player
+    #0,1,2 determines type of piece
+    #3rd is black 0 or white 1 player
     #5th is position
     #6th is alive 0 or dead 1
     #7th is occupied 1 or free 0
+    #8th ?
+    
+    #Placing black pieces. Player 0
     board = np.zeros((64,8))
     board[0] = (0,1,0,0,0,0,1,1) #rook
     board[1] = (0,1,1,0,1,0,1,1) #knight
@@ -23,59 +25,63 @@ def LOAD_CHESS(): #Loads the pieces in correct positions
     board[7] = (0,1,0,0,7,0,1,1) #rook
     board[6] = (0,1,1,0,6,0,1,1) #knight
     board[5] = (1,0,0,0,5,0,1,1) #bishop
-    for p in range(8,16):
-        board[p] = (0,0,1,0,p,0,1,1) #pawns
- 
     
-    #Placing white pieces
-    board[-1] = (0,1,0,1,63,0,1,1) #rook #The second rook
-    board[-2] = (0,1,1,1,62,0,1,1) #knight
-    board[-3] = (1,0,0,1,61,0,1,1) #bishop
-    board[-5] = (1,1,1,1,59,0,1,1) #king
-    board[-4] = (1,1,0,1,60,0,1,1) #queen
-    board[-8] = (0,1,0,1,56,0,1,1) #rook #The first rook
-    board[-7] = (0,1,1,1,57,0,1,1) #knight
-    board[-6] = (1,0,0,1,58,0,1,1) #bishop
+    for p in range(8,16):
+        board[p] = (0,0.5,0.5,0,p,0,1,1) #pawns
+    
+    
+    #Placing white pieces. Player 1
+    board[63] = (0,1,0,1,63,0,1,1) #rook #The second rook
+    board[62] = (0,1,1,1,62,0,1,1) #knight
+    board[61] = (1,0,0,1,61,0,1,1) #bishop
+    board[60] = (1,1,1,1,59,0,1,1) #king
+    board[59] = (1,1,0,1,60,0,1,1) #queen
+    board[56] = (0,1,0,1,56,0,1,1) #rook #The first rook
+    board[57] = (0,1,1,1,57,0,1,1) #knight
+    board[58] = (1,0,0,1,58,0,1,1) #bishop
+    
     for p in range(48,56):
         board[p] = (0,0,1,1,p,0,1,1) #pawns
     
     return board
 
 
-def check_pos(check_loc, legal_moves, moves, piece_id, board, player, piece=''):
+def check_pos(current_loc,check_loc, legal_moves, moves, piece_id, board, player, piece=''):
     #print("Checking pos", check_loc, piece_id)
     
+    # check_loc: The location we determine is occupied or free
+    # legal_moves: list of legal moves for this piece
+    # We must add boundaries
+    
     skip = 0
-    if check_loc >= 64 or check_loc < 0:
+    if check_loc not in range(64):
         "illegal move. skipping"
-        print(check_loc)
-        check_loc = 63
+        return legal_moves, moves, 0
         
     if moves >= 27:
-        print(moves)
-        moves = 26
+        "Moves is the legal tile placement in the list of legal moves"
+        moves = 26 
     
     else:
-        if board[ check_loc , -1] == 0: #Checks if immediate tile is free or occupied
-            
-            
-            
+        "If the number of legal moves are not exhausted"
+        if board[ check_loc , -1] == 0: #Check the proposed tile is free or occupied
             legal_moves[piece_id, moves ] = check_loc
             moves+=1
-            
-        elif board[ check_loc , -1] == 1 and board[ check_loc , 3] == np.mod( player+ 1, 2):
-            
-            if piece == 'pawn': #pawn cannot capture pieces in front of it
-                return legal_moves, moves, 0
-            
-            else:
-                #Capture enemy piece
-                legal_moves[piece_id, moves] = check_loc
-                moves+=1
-                #print("Captured enemy piece at ", check_loc)
-                return legal_moves, moves, 1
-        else:
+            return legal_moves, moves, 0
+        
+        
+        if board[ check_loc, -1 ] == 1 and board[ check_loc , 3] == player and piece != 'pawn':
+            "if the tile is occupied by the same team"
             return legal_moves, moves, 1
+        
+        
+        if board[ check_loc , -1] == 1 and board[ check_loc , 3] == np.mod( player+ 1, 2) and piece != 'pawn':
+            "If the proposed tile is occupied by the opponent"
+            "Capture enemy piece. Except for pawn"
+            legal_moves[piece_id, moves] = check_loc
+            moves+=1
+            return legal_moves, moves, 1
+
     
     return legal_moves, moves, 0
 
@@ -87,20 +93,44 @@ def PAWN(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, mo
     current_row = int( np.floor(current_loc / 8)   )   #Zero based
     current_col = int( current_loc - 8*current_row )   #Zero based
     STOP = 0
-    for i in range(1,2): #Checking topside
+
+    check_loc = current_loc + 8 - player * 16
+    check_left  = check_loc - 1
+    check_right = check_loc + 1
+    
+    if STOP!=1:
+        # Checks up/down
+        legal_moves, moves, STOP = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player, piece='pawn')
         
-        if player == 0: #We go down
-            check_loc = current_loc + 8 #(current_row-i)*8+(current_col)
-
-        else: #We go up
-            check_loc = current_loc - 8 #(current_row+i)*8+(current_col)
-            
-        #print("current loc: ", current_loc, "cheking loc:",check_loc, "player: ", player)
-        if STOP!=1: # and current_col > col:
-            legal_moves, moves, STOP = check_pos(check_loc, legal_moves, moves, piece_id, board, player, piece='pawn')
-
-    #print(legal_moves)
+    if check_loc > 0 and check_loc < 64:
+        if board[ check_loc, -1 ] == 0:
+            "Tile is empty. We may move"
+            legal_moves, moves, STOP = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player, piece='pawn')
+        
+    "We need to check if check_left and check_right are within col and row limits"
+    check_row       = np.floor( check_loc / 8 )
+    # Must be positive values
+    check_col_left  = int( check_left  - 8 * check_row ) 
+    check_col_right = int( check_right - 8 * check_row )
+    
+    "If player == 1: We go up by reducing the board values. Otherwise we go down by increasing board values"
+    left  = current_loc + 7 - player*16
+    right = current_loc + 9 - player*16
+    if left > 0 and left < 64 and check_col_left > 0:
+        if board[ left , -1] == 1 and board[ left , 3] == np.mod( player + 1, 2):
+            "Pawn captures enemy in front-side"
+            legal_moves[piece_id, moves] = left
+            moves+=1
+    
+    if right > 0 and right < 64 and check_col_right > 0:
+        if board[ right , -1] == 1 and board[ right , 3] == np.mod( player + 1, 2):
+            "Pawn captures enemy in front-side"
+            legal_moves[piece_id, moves] = right
+            moves+=1
+    
     return board, moves, legal_moves
+
+
 
 def ROOK(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, moves=0):
     #The rook can go only updown and leftright
@@ -124,35 +154,40 @@ def ROOK(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, mo
     
     STOP_right = 0
     STOP_left = 0
-
     STOP_top = 0
     STOP_bot = 0
-    for i in range(1,steps): #Checking topside
-        
-        check_right = (current_row)*8+(current_col) +i
-        
-        check_left = (current_row)*8+(current_col) -i
-        
-        
-        if check_right >= 0 and check_right < 64 and STOP_right != 0: # and current_col > col:
-            legal_moves, moves, STOP_right = check_pos(check_right, legal_moves, moves, piece_id, board, player)
     
-        if check_left < 64 and check_left >= 0 and STOP_left != 1:
-            legal_moves, moves, STOP_left = check_pos(check_left, legal_moves, moves, piece_id, board, player)
+    for i in range(1,steps): #Checking topside
+    
+        "We must add boundaries for the rook"
+        "Id we are within the boundaries, we're allowed to move"
+        "We have to check if the tiles on our left/right and up/down are available"
+        
+        check_right = current_col + i   # Check right and left limits. Must be >= 0
+        check_left  = current_col - i
+        
+        if check_right in range(8) and STOP_right != 1: # and current_col > col:
+            
+            check_loc = current_row*8 + check_right
+            legal_moves, moves, STOP_right = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+    
+        if check_left in range(8) and STOP_left != 1:
+
+            check_loc = current_row*8 + check_left
+            legal_moves, moves, STOP_left = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+
+        check_bot = current_row + i
+        check_top = current_row - i
+        
+        if check_bot in range(8) and STOP_bot != 1:
+            check_loc = check_bot*8 + current_col
+            legal_moves, moves, STOP_bot = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
 
         
-        check_top = (current_row +i)*8+(current_col)
+        if check_top in range(8) and STOP_top != 1: #failsafe
+            check_loc =  check_top*8 + current_col
+            legal_moves, moves, STOP_top = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
         
-        check_bot = (current_row -i)*8+(current_col) 
-        
-        if check_top >= 0 and check_top < 64 and STOP_top != 1: #failsafe
-            legal_moves, moves, STOP_top = check_pos(check_top, legal_moves, moves, piece_id, board, player)
-        
-        if check_bot < 64 and check_bot >= 0 and STOP_bot != 1:
-            legal_moves, moves, STOP_bot = check_pos(check_bot, legal_moves, moves, piece_id, board, player)
-    #print(legal_moves)
-
-
     return board, moves, legal_moves
 
 
@@ -166,64 +201,70 @@ def BISHOP(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, 
     #These two rows of code translate our current position into X and Y axis positions
     current_row = int( np.floor(current_loc / 8)   )   #Zero based
     current_col = int( current_loc - 8*current_row )   #Zero based
-
     
+        
     if piece == "king":
         steps = 1
     else:
         steps = 8 #Maximum number of steps
     
-
     STOP_UR = 0
     STOP_UL = 0
-    
     STOP_DR = 0
     STOP_DL = 0
-        
+    
     for i in range(1,steps): #Checking topside
 
         ###UP RIGHT #Correct
         #to do next: add rows and cols to keep boundaries
         check_loc = (current_row-i)*8+(current_col) +i #Goes one step up and then one step right
-        row = int( np.floor(check_loc / 8)   )   #Zero based
-        col = int( check_loc - 8*row )   #Zero based
-        if check_loc >= 0 and STOP_UR != 1 and row >= 0 and col < 8:
-            legal_moves, moves, STOP_UR = check_pos(check_loc, legal_moves, moves, piece_id, board, player)
-            
+        row = current_row - i
+        col = current_col + i
+        
+        if row in range(8) and col in range(8) and check_loc in range(64) and STOP_UR != 1:
+            #print(row, col)
+            legal_moves, moves, STOP_UR = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+        
+        
         ###UP LEFT #Correct
         check_loc = (current_row-i)*8+(current_col) -i
-        row = int( np.floor(check_loc / 8)   )   #Zero based
-        col = int( check_loc - 8*row )   #Zero based
-        if check_loc >= 0 and STOP_UL != 1 and row >= 0 and col >= 0 and current_col > col:
-            legal_moves, moves,  STOP_UL = check_pos(check_loc, legal_moves, moves, piece_id, board, player)
-            
+        row = current_row - i
+        col = current_col - i
         
+        if check_loc in range(64) and STOP_UL != 1:
+            if row in range(8) and col in range(8):
+                legal_moves, moves,  STOP_UL = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+                
         ###Down right #Correct
         check_loc = (current_row+i)*8+(current_col) +i
-        row = int( np.floor(check_loc / 8)   )   #Zero based
-        col = int( check_loc - 8*row )   #Zero based
-        if check_loc < 64 and STOP_DR != 1 and row < 8 and col < 8:
-            legal_moves, moves, STOP_DR = check_pos(check_loc, legal_moves, moves, piece_id, board, player)
-            
-            
+        row = current_row + i
+        col = current_col + i
+        
+        if check_loc in range(64) and STOP_DR != 1:
+            if row in range(8) and col in range(8):
+                legal_moves, moves, STOP_DR = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+        
+        
         ###Down Left #Correct
         check_loc = (current_row+i)*8+(current_col) -i
-        row = int( np.floor(check_loc / 8)   )   #Zero based
-        col = int( check_loc - 8*row )   #Zero based
-        if check_loc >= 0 and check_loc < 64 and STOP_DL != 1 and row < 8 and col >= 0 and current_col > col:
-            legal_moves, moves, STOP_DL = check_pos(check_loc, legal_moves, moves, piece_id, board, player)
-            
+        row = current_row + i
+        col = current_col - i
+        
+        if check_loc in range(64) and STOP_DR != 1:
+            if row in range(8) and col in range(8):
+                legal_moves, moves, STOP_DL = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
+        
         
     return board, moves, legal_moves
 
 
 
 def KING_QUEEN(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, moves=0):
-
-
+    
+    
     board, moves, legal_moves = ROOK(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves)
     board, moves, legal_moves = BISHOP(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, moves=moves)
-
+    
     
     return board, moves, legal_moves
 
@@ -264,18 +305,28 @@ def KNIGHT(player, piece, piece_id, current_loc, board, AV_pieces, legal_moves, 
         
         check_loc = (current_row+ mod_row[i%4]*switch )*8 + (current_col + mod_col[i%4]*switch )
         
-        row = int( np.floor(check_loc / 8)   )   #Zero based
-        col = int( check_loc - 8*row )   #Zero based
+        row = int( np.floor(check_loc / 8))   #Zero based
+        col = int( check_loc - 8*row      )   #Zero based
         
         
         if check_loc >= 0 and check_loc <= 63:
-            legal_moves, moves, STOP_DL = check_pos(check_loc, legal_moves, moves, piece_id, board, player)
+            legal_moves, moves, STOP_DL = check_pos(current_loc, check_loc, legal_moves, moves, piece_id, board, player)
     
     
     
     return board, moves, legal_moves
     
     
+
+
+
+
+
+
+
+
+
+
 
 
 
